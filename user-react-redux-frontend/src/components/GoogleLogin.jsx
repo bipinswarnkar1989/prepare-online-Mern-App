5d8e1e3d8afbccf17c4aabbfd5e8e63561494ae0
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2017-12-07 23:14:19 +0530

    added fetching user data from google plus api

--- user-react-redux-frontend/src/components/GoogleLogin.jsx
+++ user-react-redux-frontend/src/components/GoogleLogin.jsx
@@ -15,7 +15,7 @@ class GoogleLogin extends React.Component{
         window.onLoadCallback = function(){
              window.gapi.load('client:auth2', self.initClient());
            }
-
+     this.updateSigninStatus = this.updateSigninStatus.bind(this);
 
     }
 
@@ -27,51 +27,113 @@ class GoogleLogin extends React.Component{
      }
 
       initClient() {
+        const self = this;
        // Initialize the client with API key and People API, and initialize OAuth with an
        // OAuth 2.0 client ID and scopes (space delimited string) to request access.
        window.gapi.client.init({
-           apiKey: 'kSa6D8sIMUAg1ZwXKF3iCTUv',
+           apiKey: 'AIzaSyDwIfkn-PbCZiNfL8QUAVAbHNfy2H5ll98',
            discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
-           clientId: '589604253674-6ht3ue6eeqsui6142cvc96sr9pdcljot.apps.googleusercontent.com',
+           clientId: '589604253674-5p72onvt84t9l3ev7lrav6i9c1je9i5t.apps.googleusercontent.com',
            scope: 'profile'
        }).then(function () {
          // Listen for sign-in state changes.
-         window.gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus());
+         window.gapi.auth2.getAuthInstance().isSignedIn.listen(self.updateSigninStatus);
 
-         // Handle the initial sign-in state.
-         this.updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get());
+         // Handle the initial sign-in state. But not needed . by bipin. so commenting
+         //self.updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get());
        });
      }
 
       updateSigninStatus(isSignedIn) {
        // When signin status changes, this function is called.
        // If the signin status is changed to signedIn, we make an API call.
-       if (isSignedIn) {
-         this.makeApiCall();
+       if (isSignedIn) {console.log('updateSigninStatus')
+         window.gapi.client.load('plus','v1',function(){
+           var request = window.gapi.client.plus.people.get({
+             'userId':'me'
+           });
+           request.execute(function(resp){
+             console.log(resp);
+             console.log('Retrieved profile for: '+resp.displayName);
+           })
+         })
        }
      }
 
       handleSignInClick(event) {
+        event.preventDefault();
        // Ideally the button should only show up after gapi.client.init finishes, so that this
        // handler won't be called before OAuth is initialized.
-       window.gapi.auth2.getAuthInstance().signIn();
+       this.googleLogin();
+       //window.gapi.auth2.getAuthInstance().signIn();
      }
 
       handleSignOutClick(event) {
        window.gapi.auth2.getAuthInstance().signOut();
      }
 
-      makeApiCall() {
+      makeApiCall() {console.log('makeApiCall')
        // Make an API call to the People API, and print the user's given name.
        window.gapi.client.people.people.get({
          'resourceName': 'people/me',
-         'requestMask.includeField': 'person.names'
+         'requestMask.includeField': 'person.names,person.emailAddresses,person.phoneNumbers',
        }).then(function(response) {
+         console.log(response.result)
          console.log('Hello, ' + response.result.names[0].givenName);
        }, function(reason) {
          console.log('Error: ' + reason.result.error.message);
        });
      }
+
+     //Triggering login for google
+    googleLogin = () => {
+        let response = null;
+        window.gapi.auth.signIn({
+            callback: function(authResponse) {
+                this.googleSignInCallback( authResponse )
+            }.bind( this ),
+            clientid: '589604253674-5p72onvt84t9l3ev7lrav6i9c1je9i5t.apps.googleusercontent.com', //Google client Id
+            cookiepolicy: "single_host_origin",
+            requestvisibleactions: "http://schema.org/AddAction",
+            scope: "https://www.googleapis.com/auth/plus.login email"
+        });
+    }
+
+    googleSignInCallback = (e) => {
+        console.log( e )
+        if (e["status"]["signed_in"]) {
+            window.gapi.client.load("plus", "v1", function() {
+                if (e["access_token"]) {
+                    this.getUserGoogleProfile( e["access_token"] )
+                } else if (e["error"]) {
+                    console.log('Import error', 'Error occured while importing data')
+                }
+            }.bind(this));
+        } else {
+            console.log('Oops... Error occured while importing data')
+        }
+    }
+
+    getUserGoogleProfile = accesstoken => {
+        var e = window.gapi.client.plus.people.get({
+            userId: "me"
+        });
+        e.execute(function(e) {
+            if (e.error) {
+                console.log(e.message);
+                console.log('Import error - Error occured while importing data')
+                return
+
+            } else if (e.id) {
+                //Profile data
+                console.log(e)
+                alert("Successfull login from google : "+ e.displayName );
+                return;
+            }
+        }.bind(this));
+    }
+
+
 
 
 
