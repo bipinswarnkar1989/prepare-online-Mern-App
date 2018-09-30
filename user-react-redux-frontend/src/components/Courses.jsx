9ebcb5e4de151eeefe4f4c13b69a640420ed5c20
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-09-30 23:57:04 +0530

    Started Resposive Course List

--- user-react-redux-frontend/src/components/Courses.jsx
+++ user-react-redux-frontend/src/components/Courses.jsx
@@ -7,6 +7,7 @@ import {Card, CardHeader} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import Snackbar from 'material-ui/Snackbar';
 import Loading from './Loading';
+import '.././styles/Courses.css';
 
 const apiUrl = `http://localhost:3001/api`;
 
@@ -42,11 +43,20 @@ class Courses extends React.Component {
                alignContent: 'center',
                flexDirection: 'column',
             },
-            formDiv:{
-              display:'flex',
-              flexDirection: 'column',
-              padding: 50,
+            coursesDiv:{
+            //   display:'flex',
+            //   flexDirection: 'row',
+            //   flexWrap: 'wrap',
             },
+            courseDiv:{
+                // flex:'25%',
+                // maxWidth:'25%',
+                // backgroundColor: '#f1f1f1',
+                // margin: 10,
+                // textAlign: 'center',
+                // height: 200,
+                // fontSize: 30
+            }
           
         }
         const { snackOpen, success } = this.state;
@@ -56,7 +66,65 @@ class Courses extends React.Component {
             <div style={styles.container}>
             <div style={styles.innerContainer}>
             <h3 align="center">Courses </h3>
-            
+            <div className='coursesDiv' style={styles.coursesDiv}>
+            <div className='courseDiv' style={styles.courseDiv}>
+            <div className='courseContainer'>
+            <div className='imgDiv'>
+            <img src='https://d2eip9sf3oo6c2.cloudfront.net/series/square_covers/000/000/228/square_480/EGH_ReactStorybook_Final.png' />
+            </div>
+            <div className='courseTypeDiv'>
+            <h5>COURSE <span>:</span> REACT</h5>
+            </div>
+            <div className='courseNameDiv'>
+                <span>Design Systems with React and Typescript in Storybook</span>
+            </div>
+            <div className='userDiv'>
+                <div className='userLeft'>
+                   <img src='https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/211/square_64/swyx_ski.jpg'/>
+                   <span>
+                       Bipin Swarnkar
+                   </span>
+                </div>
+                <div className='userRight'>
+                    <img src='https://egghead.io/webpack/953a4599ba2243418484ce73333fb9c1.svg'/>
+                </div>
+            </div>
+            </div>
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            <div className='courseDiv' style={styles.courseDiv}>
+            1
+            </div>
+            </div>
             </div>
             <Snackbar
           open={snackOpen}
